0c97013e1b3c0257620da416006191f043f12a71
omar <omar.a.farouk22>
2023-08-26 09:19:37 -0700

    update

--- monty_stack.c
+++ monty_stack.c
@@ -72,6 +72,9 @@ void pint(stack_t **stack, unsigned int line_number)
 	if ((*stack) == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_all();
+		free_stack(stack);
+		fclose(glob_var.file);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
